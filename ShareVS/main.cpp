9e537dde347b92f3184ebf2615763e194d6dab6c
Seiya-Ito <s-it.aoyama@hotmail.co.jp>
2015-02-23 17:36:04 +0900

    Initial Commit

---
+++ ShareVS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShareVS
+//
+//  Created by Seiya Ito on 2015/02/23.
+//  Copyright (c) 2015年 Seiya Ito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
